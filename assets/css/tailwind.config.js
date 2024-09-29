107f2778154ee2dd3e71ba256c95ea97b70da65b
Pid24 <rofidnasifannafie@gmail.com>
2024-09-29 07:09:37 +0700

    first commit

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#010003',
+          secondary:'#121316',
+          paragraph: '#626262',
+          btn_blue: '#1880E8',
+          username: '#5E6368',
+          link: '#1880E8',
+          line: '#1E1E1E',
+          like: '#F91880'
+        },
+      fontFamily: {
+          'poppins': ['Poppins', 'sans-serif'],
+      },
+      }
+    }
+  }
